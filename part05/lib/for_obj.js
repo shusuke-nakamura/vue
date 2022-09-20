2886a0d3c21ab496989f6a6048605c5b4959ce82
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-09-21 01:26:44 +0900

    5.3.4 数値を列挙したい場合の学習完了

---
+++ part05/lib/for_obj.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+    data() {
+        return {
+            member: {
+                name: '山田太郎',
+                age: 30,
+                mail: 'yamada@example.com'
+            }
+        }
+    },
+}).mount('#app')
